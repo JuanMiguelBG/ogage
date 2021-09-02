a4b25b5b16ec1007aa41207aa1eb38bf43335534
JuanMiguelBG <bacojmbg@gmial.com>
2021-09-02 08:20:43 +0000

    Add manage of double push powerkey to shutdown

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,9 @@ use std::os::unix::io::AsRawFd;
 use mio::{Poll,Events,Token,Interest};
 use mio::unix::SourceFd;
 use std::fs;
+use props_rs::*;
+use libc::time_t;
+use std::collections::HashMap;
 
 static PERF_MAX:    EventCode = EventCode::EV_KEY(EV_KEY::BTN_TL2);
 static PERF_NORM:   EventCode = EventCode::EV_KEY(EV_KEY::BTN_TL);
@@ -20,11 +23,12 @@ static DARK_ON:     EventCode = EventCode::EV_KEY(EV_KEY::BTN_DPAD_LEFT);
 static DARK_OFF:    EventCode = EventCode::EV_KEY(EV_KEY::BTN_DPAD_RIGHT);
 static WIFI_ON:     EventCode = EventCode::EV_KEY(EV_KEY::BTN_TR);
 static WIFI_OFF:    EventCode = EventCode::EV_KEY(EV_KEY::BTN_TR2);
+static POWER_OFF:   EventCode = EventCode::EV_KEY(EV_KEY::KEY_POWER);
+static MIN_POWERKEY_DELAY: time_t = 1;
 
 lazy_static! {
     static ref DEVICE: &'static str = {
-        let mut lines = fs::read_to_string("/opt/.retrooz/device").expect("Can't read file '/opt/.retrooz/device'.");
-        lines = lines.trim_end_matches(&['\r', '\n'][..]).to_string();
+        let lines = fs::read_to_string("/opt/.retrooz/device").expect("Can't read file '/opt/.retrooz/device'.").trim_end_matches(&['\r', '\n'][..]).to_string();
         if lines.is_empty() {
             return "rgb10maxtop";
         }
@@ -51,7 +55,7 @@ lazy_static! {
         EventCode::EV_KEY(EV_KEY::BTN_TRIGGER_HAPPY6)
     };
 
-    static ref BRIGHT_UP:   EventCode = {
+    static ref BRIGHT_UP: EventCode = {
         if *IS_OGA1 {
             return EventCode::EV_KEY(EV_KEY::BTN_TRIGGER_HAPPY5);
         }
@@ -69,7 +73,7 @@ lazy_static! {
         EventCode::EV_KEY(EV_KEY::BTN_DPAD_DOWN)
     };
 
-    static ref VOL_UP:      EventCode = {
+    static ref VOL_UP: EventCode = {
         if *IS_OGA1 {
             return EventCode::EV_KEY(EV_KEY::BTN_TRIGGER_HAPPY3);
         }
@@ -78,7 +82,7 @@ lazy_static! {
         EventCode::EV_KEY(EV_KEY::BTN_NORTH)
     };
 
-    static ref VOL_DOWN:    EventCode = {
+    static ref VOL_DOWN: EventCode = {
         if *IS_OGA1 {
             return EventCode::EV_KEY(EV_KEY::BTN_TRIGGER_HAPPY2);
         }
@@ -87,7 +91,7 @@ lazy_static! {
         EventCode::EV_KEY(EV_KEY::BTN_SOUTH)
     };
 
-    static ref MUTE:        EventCode = {
+    static ref MUTE: EventCode = {
         if *IS_OGA1 {
             return EventCode::EV_KEY(EV_KEY::BTN_DPAD_DOWN);
         }
@@ -96,7 +100,7 @@ lazy_static! {
         EventCode::EV_KEY(EV_KEY::BTN_WEST)
     };
 
-    static ref VOL_NORM:    EventCode = {
+    static ref VOL_NORM: EventCode = {
         if *IS_OGA1 {
             return EventCode::EV_KEY(EV_KEY::BTN_DPAD_UP);
         }
@@ -105,7 +109,7 @@ lazy_static! {
         EventCode::EV_KEY(EV_KEY::BTN_EAST)
     };
 
-    static ref SUSPEND:     EventCode = {
+    static ref SUSPEND: EventCode = {
         if *IS_OGA1 {
             return EventCode::EV_KEY(EV_KEY::BTN_NORTH);
         }
@@ -116,10 +120,48 @@ lazy_static! {
         //if (DEVICE == "ogs") or (DEVICE == "oga") or (DEVICE == "rgb10maxtop")
         EventCode::EV_KEY(EV_KEY::BTN_TRIGGER_HAPPY2)
     };
+
+    static ref POWERKEY_PROPERTIES: HashMap<String, String> = {
+        let lines = fs::read_to_string("/usr/local/etc/powerkey.conf").expect("Can't read file '/usr/local/etc/powerkey.conf'.");
+        let parsed = parse(lines.as_bytes()).expect("Can't parse properties of '/usr/local/etc/powerkey.conf'");
+        
+        to_map(parsed)
+    };
+
+    static ref IS_DOUBLE_PUSH_POWER_OFF_ACTIVE: bool = {       
+        println!("POWERKEY_PROPERTIES: {}", POWERKEY_PROPERTIES.len());
+        for (key, value) in POWERKEY_PROPERTIES.iter() {
+            println!("{} / {}", key, value);
+        }
+
+        if !POWERKEY_PROPERTIES.is_empty() {
+            match POWERKEY_PROPERTIES.get("two_push_shutdown") {
+                Some(x) => {
+                    if x == "enabled" {
+                        return true;
+                    }
+                },
+                None => return false,
+            };
+        }
+
+        false
+    };
+
+    static ref MAX_POWERKEY_DELAY: time_t = {
+        if !POWERKEY_PROPERTIES.is_empty() {
+            match POWERKEY_PROPERTIES.get("max_delay_time") {
+                Some(x) => return x.parse::<i64>().unwrap(),
+                None => return 2,
+            };
+        }
+
+        2
+    };
+
 }
 
 fn blinkon() {
-
     let output = Command::new("brightnessctl").arg("g").stdout(Stdio::piped()).output().expect("Failed to execute brightnessctl");
     let current = String::from_utf8(output.stdout).unwrap();
     Command::new("brightnessctl").args(&["s","0"]).output().expect("Failed to execute brightnessctl");
@@ -132,7 +174,6 @@ fn blinkon() {
 }
 
 fn blinkoff() {
-
     let output = Command::new("brightnessctl").arg("g").stdout(Stdio::piped()).output().expect("Failed to execute brightnessctl");
     let current = String::from_utf8(output.stdout).unwrap();
     Command::new("brightnessctl").args(&["s","0"]).output().expect("Failed to execute brightnessctl");
@@ -193,24 +234,33 @@ fn wifi_off() {
 }
 
 fn suspend() {
-    Command::new("sudo").args(&["systemctl", "suspend"]).output().expect("Failed to execute power off");
+    Command::new("sudo").args(&["systemctl", "suspend"]).output().expect("Failed to execute suspend");
 }
 
-fn process_event(_dev: &Device, ev: &InputEvent, hotkey: bool) {
-/*    println!("Event: time {}.{} type {} code {} value {} hotkey {}",
+fn power_off() {
+    Command::new("sudo").args(&["shutdown", "-h", "now"]).output().expect("Failed to execute power off");
+}
+
+fn process_event(_dev: &Device, ev: &InputEvent, hotkey: bool, _first_push_power_off: time_t) {
+/*
+        println!("Event: time {}.{} type {} code {} value {} hotkey {}",
              ev.time.tv_sec,
              ev.time.tv_usec,
              ev.event_type,
              ev.event_code,
              ev.value,
-             hotkey);*/
+             hotkey);
+*/
 
     if ev.value == 1 {
-/*        println!("Event: time {}.{} type {} code {} value {} hotkey {}",
-        ev.time.tv_sec, ev.time.tv_usec, ev.event_type, ev.event_code,
-        ev.value, hotkey);
-        println!("Device: {}", *DEVICE);*/
-
+/*
+        println!("Event: time {}.{} type {} code {} value {} hotkey {}",
+            ev.time.tv_sec, ev.time.tv_usec, ev.event_type, ev.event_code,
+            ev.value, hotkey);
+        println!("Device: {}", *DEVICE);
+        println!("Is OGA v1.1: {}", *IS_OGA1);
+        println!("IS double push power off button active?: {}", *IS_DOUBLE_PUSH_POWER_OFF_ACTIVE);
+*/
         if hotkey {
             if !*IS_OGA1 {
                 if ev.event_code == *BRIGHT_UP {
@@ -268,6 +318,13 @@ fn process_event(_dev: &Device, ev: &InputEvent, hotkey: bool) {
                 dec_volume();
             } 
 		}
+        else if ev.event_code == POWER_OFF && *IS_DOUBLE_PUSH_POWER_OFF_ACTIVE {
+            let diff = ev.time.tv_sec - _first_push_power_off;
+            println!("ev.time.tv_sec: {} - _first_push_power_off: {} = {}", ev.time.tv_sec, _first_push_power_off, diff);
+            if diff >= MIN_POWERKEY_DELAY && diff < *MAX_POWERKEY_DELAY { // two push in one second
+                power_off();
+            }
+        }
 	}
 }
 
@@ -276,8 +333,11 @@ fn main() -> io::Result<()> {
     let mut events = Events::with_capacity(1);
     let mut devs: Vec<Device> = Vec::new();
     let mut hotkey = false;
+    let mut first_push_power_off: time_t = 0;
 
     println!("Device: {}", *DEVICE);
+    println!("Is OGA v1.1: {}", *IS_OGA1);
+    println!("Is double push power off button active?: {}", *IS_DOUBLE_PUSH_POWER_OFF_ACTIVE);
 
     let mut i = 0;
     for s in ["/dev/input/event3", "/dev/input/event2", "/dev/input/event0", "/dev/input/event1"].iter() {
@@ -311,7 +371,11 @@ fn main() -> io::Result<()> {
                             //let grab = if hotkey { GrabMode::Grab } else { GrabMode::Ungrab };
                             //dev.grab(grab)?;
                         }
-                        process_event(&dev, &ev, hotkey)
+                        process_event(&dev, &ev, hotkey, first_push_power_off);
+                        if ev.event_code == POWER_OFF && *IS_DOUBLE_PUSH_POWER_OFF_ACTIVE {
+                            first_push_power_off = ev.time.tv_sec;
+                            //println!("new first_push_power_off: {}", first_push_power_off);
+                        }
                     },
                     _ => ()
                 }
